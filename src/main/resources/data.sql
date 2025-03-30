1b49872a5369859ecef09e7d0c7d470f3d05c13f
jueun275 <52851721+jueun275@users.noreply.github.com>
2025-03-30 15:38:19 +0900

    feat: data.sql 파일을 이용해 AccountUser 초기 데이터 삽입

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO account_user (user_name, created_at, updated_at)
+VALUES ('testUser1', NOW(), NOW());
+
+INSERT INTO account_user (user_name, created_at, updated_at)
+VALUES ('testUser2', NOW(), NOW());
+
+INSERT INTO account_user (user_name, created_at, updated_at)
+VALUES ('testUser3', NOW(), NOW());
